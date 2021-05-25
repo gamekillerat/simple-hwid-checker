51943d2eee2deab7efb44fe713a3ec4e4c4bd58c
gamekillerat <81649441+gamekillerat@users.noreply.github.com>
2021-05-25 20:44:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Simple_HWID_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hwid = System.Security.Principal.WindowsIdentity.GetCurrent().User.Value;
+            Console.WriteLine("Your HWID is: " + hwid);
+            Console.ReadKey();
+        }
+    }
+}
